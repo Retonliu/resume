a92c2f9f29a4ba9277c0e5852a374999d89d1819
Retonliu <Reton@foxmail.com>
2020-10-05 18:56:24 +0800

    完成项目

--- script/resume.js
+++ script/resume.js
@@ -1,6 +1,11 @@
 var tab = document.getElementsByClassName("tab")[0];
 var edu = document.getElementsByClassName("education")[0];
 var work = document.getElementsByClassName("work")[0];
+var content = document.getElementById("content");
+var btn = document.getElementsByTagName("button")[0];
+var userText = document.getElementById("userText");
+var userName = document.getElementById("userName");
+var index = 1;
 tab.addEventListener("click", function(event) {
     let target = event.target;
     if (target.innerText === "教育经历") { //点击“教育经历”
@@ -32,4 +37,29 @@ tab.addEventListener("click", function(event) {
             edu.classList.add("hide");
         }
     }
+}, false);
+btn.addEventListener("click", function(){
+    let text = userText.value;
+    let name = userName.value;
+    let container = document.createElement("div");
+    let picture = document.createElement("img");
+    let nameP = document.createElement("p");
+    let textP = document.createElement("p");
+
+    nameP.innerText = name;
+    textP.innerText = text;
+    nameP.classList.add("name");
+    textP.classList.add("text");
+    if (index % 2 === 0) { //偶数
+        container.classList.add("evenComment");
+    } else {
+        container.classList.add("oddComment");
+    }
+    picture.src = "style/img/smile.jpg";
+    picture.alt = "simon";
+    container.appendChild(picture);
+    container.appendChild(nameP);
+    container.appendChild(textP);
+    content.insertBefore(container, content.firstChild); //作为第一个节点插入
+    index += 1;
 }, false);
